729c797f20bb6ff18eb3292871089094e3da206d
sarooj bukhari <sbukhari@fort313.com>
2022-01-29 08:28:15 +0500

    ist commit

---
+++ src/lib/components/Swap/SwapButton.tsx
@@ -0,0 +1,172 @@
+import { BigNumber } from '@ethersproject/bignumber'
+import { Trans } from '@lingui/macro'
+import { Token, TradeType } from '@uniswap/sdk-core'
+import { CHAIN_INFO } from 'constants/chainInfo'
+import useCurrentBlockTimestamp from 'hooks/useCurrentBlockTimestamp'
+import { useERC20PermitFromTrade } from 'hooks/useERC20Permit'
+import { useUpdateAtom } from 'jotai/utils'
+import { useAtomValue } from 'jotai/utils'
+import { useSwapInfo } from 'lib/hooks/swap'
+import useSwapApproval, {
+  ApprovalState,
+  useSwapApprovalOptimizedTrade,
+  useSwapRouterAddress,
+} from 'lib/hooks/swap/useSwapApproval'
+import { useSwapCallback } from 'lib/hooks/swap/useSwapCallback'
+import { useAddTransaction } from 'lib/hooks/transactions'
+import { usePendingApproval } from 'lib/hooks/transactions'
+import useActiveWeb3React from 'lib/hooks/useActiveWeb3React'
+import { Link, Spinner } from 'lib/icons'
+import { transactionTtlAtom } from 'lib/state/settings'
+import { displayTxHashAtom, Field, independentFieldAtom } from 'lib/state/swap'
+import { TransactionType } from 'lib/state/transactions'
+import styled from 'lib/theme'
+import { useCallback, useEffect, useMemo, useState } from 'react'
+import invariant from 'tiny-invariant'
+
+import ActionButton from '../ActionButton'
+import Dialog from '../Dialog'
+import Row from '../Row'
+import { SummaryDialog } from './Summary'
+
+interface SwapButtonProps {
+  disabled?: boolean
+}
+
+const EtherscanA = styled.a`
+  color: currentColor;
+  text-decoration: none;
+`
+
+function useIsPendingApproval(token?: Token, spender?: string): boolean {
+  return Boolean(usePendingApproval(token, spender))
+}
+
+export default function SwapButton({ disabled }: SwapButtonProps) {
+  const { account, chainId } = useActiveWeb3React()
+
+  const {
+    trade,
+    allowedSlippage,
+    currencies: { [Field.INPUT]: inputCurrency },
+    currencyBalances: { [Field.INPUT]: inputCurrencyBalance },
+    currencyAmounts: { [Field.INPUT]: inputCurrencyAmount, [Field.OUTPUT]: outputCurrencyAmount },
+  } = useSwapInfo()
+
+  const independentField = useAtomValue(independentFieldAtom)
+
+  const [activeTrade, setActiveTrade] = useState<typeof trade.trade | undefined>()
+  useEffect(() => {
+    setActiveTrade((activeTrade) => activeTrade && trade.trade)
+  }, [trade])
+
+  // TODO(zzmp): Return an optimized trade directly from useSwapInfo.
+  const optimizedTrade =
+    // Use trade.trade if there is no swap optimized trade. This occurs if approvals are still pending.
+    useSwapApprovalOptimizedTrade(trade.trade, allowedSlippage, useIsPendingApproval) || trade.trade
+  const [approval, getApproval] = useSwapApproval(optimizedTrade, allowedSlippage, useIsPendingApproval)
+  const approvalHash = usePendingApproval(
+    inputCurrency?.isToken ? inputCurrency : undefined,
+    useSwapRouterAddress(optimizedTrade)
+  )
+
+  const addTransaction = useAddTransaction()
+  const addApprovalTransaction = useCallback(() => {
+    getApproval().then((transaction) => {
+      if (transaction) {
+        addTransaction({ type: TransactionType.APPROVAL, ...transaction })
+      }
+    })
+  }, [addTransaction, getApproval])
+
+  const actionProps = useMemo(() => {
+    if (disabled) return { disabled: true }
+
+    if (chainId && inputCurrencyAmount && inputCurrencyBalance?.greaterThan(inputCurrencyAmount)) {
+      if (approval === ApprovalState.PENDING) {
+        return {
+          disabled: true,
+          update: {
+            message: (
+              <EtherscanA href={approvalHash && `${CHAIN_INFO[chainId].explorer}tx/${approvalHash}`} target="_blank">
+                <Row gap={0.25}>
+                  <Trans>
+                    Approval pending <Link />
+                  </Trans>
+                </Row>
+              </EtherscanA>
+            ),
+            action: <Trans>Approve</Trans>,
+            icon: Spinner,
+          },
+        }
+      } else if (approval === ApprovalState.NOT_APPROVED) {
+        return {
+          update: {
+            message: <Trans>Approve {inputCurrencyAmount.currency.symbol} first</Trans>,
+            action: <Trans>Approve</Trans>,
+          },
+        }
+      }
+      return {}
+    }
+
+    return { disabled: true }
+  }, [approval, approvalHash, chainId, disabled, inputCurrencyAmount, inputCurrencyBalance])
+
+  // @TODO(ianlapham): connect deadline from state instead of passing undefined.
+  const { signatureData } = useERC20PermitFromTrade(optimizedTrade, allowedSlippage, undefined)
+
+  const currentBlockTimestamp = useCurrentBlockTimestamp()
+  const userDeadline = useAtomValue(transactionTtlAtom)
+  const deadline = currentBlockTimestamp?.add(BigNumber.from(userDeadline))
+
+  // the callback to execute the swap
+  const { callback: swapCallback } = useSwapCallback(
+    optimizedTrade,
+    allowedSlippage,
+    account ?? null,
+    signatureData,
+    deadline
+  )
+
+  //@TODO(ianlapham): add a loading state, process errors
+  const setDisplayTxHash = useUpdateAtom(displayTxHashAtom)
+
+  const onConfirm = useCallback(() => {
+    swapCallback?.()
+      .then((response) => {
+        setDisplayTxHash(response.hash)
+        invariant(inputCurrencyAmount && outputCurrencyAmount)
+        addTransaction({
+          response,
+          type: TransactionType.SWAP,
+          tradeType: independentField === Field.INPUT ? TradeType.EXACT_INPUT : TradeType.EXACT_OUTPUT,
+          inputCurrencyAmount,
+          outputCurrencyAmount,
+        })
+      })
+      .catch((error) => {
+        //@TODO(ianlapham): add error handling
+        console.log(error)
+      })
+  }, [addTransaction, independentField, inputCurrencyAmount, outputCurrencyAmount, setDisplayTxHash, swapCallback])
+
+  return (
+    <>
+      <ActionButton
+        color="interactive"
+        onClick={() => setActiveTrade(trade.trade)}
+        onUpdate={addApprovalTransaction}
+        {...actionProps}
+      >
+        <Trans>Review swap</Trans>
+      </ActionButton>
+      {activeTrade && (
+        <Dialog color="dialog" onClose={() => setActiveTrade(undefined)}>
+          <SummaryDialog trade={activeTrade} allowedSlippage={allowedSlippage} onConfirm={onConfirm} />
+        </Dialog>
+      )}
+    </>
+  )
+}
---
+++ src/pages/Pool/index.tsx
@@ -0,0 +1,286 @@
+import { Trans } from '@lingui/macro'
+import axios from 'axios'
+import { ButtonGray, ButtonPrimary } from 'components/Button'
+import { AutoColumn } from 'components/Column'
+import { NewMenu } from 'components/Menu'
+import { SwapPoolTabs } from 'components/NavigationTabs'
+import PositionList from 'components/PositionList'
+import { RowBetween, RowFixed } from 'components/Row'
+import { SwitchLocaleLink } from 'components/SwitchLocaleLink'
+import useActiveWeb3React from 'hooks/useActiveWeb3React'
+import { useV3Positions } from 'hooks/useV3Positions'
+import { useContext, useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { useWalletModalToggle } from 'state/application/hooks'
+import { useUserHideClosedPositions } from 'state/user/hooks'
+import styled, { ThemeContext } from 'styled-components/macro'
+import { HideSmall, ThemedText } from 'theme'
+import { PositionDetails } from 'types/position'
+
+import clock from '../../assets/svg/clock.svg'
+import uniIcon from '../../assets/svg/uniIcon.svg'
+import CTACards from './CTACards'
+import { LoadingRows } from './styleds'
+
+const baseURL = 'https://jsonplaceholder.typicode.com/posts/1'
+
+const PageWrapper = styled(AutoColumn)`
+  max-width: 870px;
+  width: 100%;
+
+  ${({ theme }) => theme.mediaWidth.upToMedium`
+    max-width: 800px;
+  `};
+
+  ${({ theme }) => theme.mediaWidth.upToSmall`
+    max-width: 500px;
+  `};
+`
+const TitleRow = styled(RowBetween)`
+  color: ${({ theme }) => theme.text2};
+  ${({ theme }) => theme.mediaWidth.upToSmall`
+    flex-wrap: wrap;
+    gap: 12px;
+    width: 100%;
+  `};
+`
+const ButtonRow = styled(RowFixed)`
+  & > *:not(:last-child) {
+    margin-left: 8px;
+  }
+
+  ${({ theme }) => theme.mediaWidth.upToSmall`
+    width: 100%;
+    flex-direction: row;
+    justify-content: space-between;
+    flex-direction: row-reverse;
+  `};
+`
+const Menu = styled(NewMenu)`
+  margin-left: 0;
+  ${({ theme }) => theme.mediaWidth.upToSmall`
+    flex: 1 1 auto;
+    width: 49%;
+    right: 0px;
+  `};
+
+  a {
+    width: 100%;
+  }
+`
+const MenuItem = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  font-weight: 500;
+`
+const MoreOptionsButton = styled(ButtonGray)`
+  border-radius: 12px;
+  flex: 1 1 auto;
+  padding: 6px 8px;
+  width: 100%;
+  background-color: ${({ theme }) => theme.bg0};
+  margin-right: 8px;
+`
+const NoLiquidity = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: auto;
+  max-width: 300px;
+  padding: 80px 0;
+`
+const ResponsiveButtonPrimary = styled(ButtonPrimary)`
+  border-radius: 12px;
+  padding: 6px 8px;
+  width: fit-content;
+  ${({ theme }) => theme.mediaWidth.upToSmall`
+    flex: 1 1 auto;
+    width: 100%;
+  `};
+`
+
+const MainContentWrapper = styled.main`
+  background-color: ${({ theme }) => theme.bg0};
+  padding: 8px;
+  border-radius: 20px;
+  display: flex;
+  flex-direction: column;
+`
+
+const NotificationBox = styled.div`
+  display: flex;
+  align-items: center;
+  border-bottom: 1.25px solid #d6d6d6;
+  margin: 0px 0 25px;
+  padding-bottom: 25px;
+`
+const Time = styled.div`
+  font-size: 12px;
+  line-height: 15px;
+  color: #98989e;
+  margin: 0,
+  display: flex,
+  align-items: center,
+`
+const NotificationText = styled.div`
+  margin: 0;
+  font-size: 18px;
+  line-height: 22px;
+  color: #434548;
+`
+const NotiContent = styled.div`
+  margin-left: 15px;
+`
+const NotificationMapWrapper = styled.div`
+  padding: 30px;
+`
+const ClockImage = styled.img`
+  margin-right: 5px;
+`
+
+function PositionsLoadingPlaceholder() {
+  return (
+    <LoadingRows>
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+    </LoadingRows>
+  )
+}
+
+export default function Pool() {
+  const { account, chainId } = useActiveWeb3React()
+  const toggleWalletModal = useWalletModalToggle()
+
+  const theme = useContext(ThemeContext)
+  const [userHideClosedPositions, setUserHideClosedPositions] = useUserHideClosedPositions()
+
+  const { positions, loading: positionsLoading } = useV3Positions(account)
+
+  const [openPositions, closedPositions] = positions?.reduce<[PositionDetails[], PositionDetails[]]>(
+    (acc, p) => {
+      acc[p.liquidity?.isZero() ? 1 : 0].push(p)
+      return acc
+    },
+    [[], []]
+  ) ?? [[], []]
+
+  const [notifications, setNotifications] = useState([
+    {
+      para: 'New Uniswap pair created: test 🦄Check out the new pool here: test ',
+      time: 'September 21 at 11:32',
+    },
+    {
+      para: 'New Uniswap pair created: test pool here: test 👯',
+      time: 'September 21 at 11:32',
+    },
+    {
+      para: 'New Uniswap pair created: test 🦄Check out the new pool here: test 👯',
+      time: 'September 21 at 11:32',
+    },
+    {
+      para: 'New Uniswap pair created: test 🦄Check out the new pool here: test 👯',
+      time: 'September 21 at 11:32',
+    },
+  ])
+
+  const filteredPositions = [...openPositions, ...(userHideClosedPositions ? [] : closedPositions)]
+  const showConnectAWallet = Boolean(!account)
+  // const showV2Features = Boolean(chainId && V2_FACTORY_ADDRESSES[chainId])
+
+  useEffect(() => {
+    if (!showConnectAWallet) {
+      axios.get(baseURL).then((response) => {
+        console.log('data', response)
+      })
+    }
+  }, [showConnectAWallet])
+
+  return (
+    <>
+      <PageWrapper>
+        <SwapPoolTabs active={'pool'} />
+        <AutoColumn gap="lg" justify="center">
+          <AutoColumn gap="lg" style={{ width: '100%' }}>
+            <TitleRow style={{ marginTop: '1rem' }} padding={'0'}>
+              <ThemedText.Body fontSize={'20px'}>
+                <Trans>Notifications</Trans>
+              </ThemedText.Body>
+              <ButtonRow>
+                <ResponsiveButtonPrimary id="join-pool-button" as={Link} to="#">
+                  + <Trans>Get Alerts</Trans>
+                </ResponsiveButtonPrimary>
+              </ButtonRow>
+            </TitleRow>
+
+            <MainContentWrapper>
+              {positionsLoading ? (
+                <PositionsLoadingPlaceholder />
+              ) : filteredPositions && closedPositions && filteredPositions.length > 0 ? (
+                <PositionList
+                  positions={filteredPositions}
+                  setUserHideClosedPositions={setUserHideClosedPositions}
+                  userHideClosedPositions={userHideClosedPositions}
+                />
+              ) : (
+                <>
+                  {!showConnectAWallet && (
+                    <>
+                      <NotificationMapWrapper>
+                        {notifications.map((item, inex) => {
+                          return (
+                            <NotificationBox key={inex}>
+                              <div className="notiIcon">
+                                <img src={uniIcon} alt="icons" />
+                              </div>
+                              <NotiContent>
+                                <Time>
+                                  <ClockImage src={clock} alt="clock"></ClockImage> {item.time}
+                                </Time>
+                                <NotificationText>{item.para}</NotificationText>
+                              </NotiContent>
+                            </NotificationBox>
+                          )
+                        })}
+                      </NotificationMapWrapper>
+                    </>
+                  )}
+                  <>
+                    {showConnectAWallet && (
+                      <NoLiquidity>
+                        <ThemedText.Body color={theme.text3} textAlign="center">
+                          <div>
+                            <Trans>Connect Wallet to see notifications</Trans>
+                          </div>
+                        </ThemedText.Body>
+                        <ButtonPrimary style={{ marginTop: '2em', padding: '8px 16px' }} onClick={toggleWalletModal}>
+                          <Trans>Connect a wallet</Trans>
+                        </ButtonPrimary>
+                      </NoLiquidity>
+                    )}
+                  </>
+                </>
+              )}
+            </MainContentWrapper>
+            <HideSmall>
+              <CTACards />
+            </HideSmall>
+          </AutoColumn>
+        </AutoColumn>
+      </PageWrapper>
+      <SwitchLocaleLink />
+    </>
+  )
+}
